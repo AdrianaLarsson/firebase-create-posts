6de0485cecaef14a7de91ee63d4d2bb1e57498ea
Adriana Larsson <adrianalarsson@Adrianas-MacBook-Air.local>
2019-09-16 14:09:29 +0200

    added genisu api key

---
+++ storage.js
@@ -0,0 +1,9 @@
+//function save teh id, title, and artist to firebase
+const saveGenius = (id, title, artist) =>{
+    return fs.collection('songs').add({
+        "id": id,
+        "title": title, 
+        "artist": artist
+    })
+}
+
